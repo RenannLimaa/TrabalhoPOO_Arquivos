190f6d62f43fd120592ca543821ee5522c41a865
renan lima <renanrod2403@gmail.com>
2023-06-07 20:11:05 -0300

    Add classe tela

---
+++ Tela.java
@@ -0,0 +1,16 @@
+public class Tela {
+    public void mostrarTelaInicial() {
+        System.out.println("1. Adicionar disciplina");
+        System.out.println("2. Consultar disciplina");
+    }
+
+    public void mostrarTelaDisciplina() {
+        System.out.println("1. Matricular alunos ");
+        System.out.println("2. Mostrar alunos matriculados");
+        System.out.println("3. Mostrar resultados da disciplina");
+    }
+
+    public void mostrarTelaAluno() {
+        //códigos aqui
+    }
+}

7012d59b4667aed26d14dbd53970df4107e6d9eb
renan lima <renanrod2403@gmail.com>
2023-06-08 16:46:09 -0300

    classe aluno added

---
+++ Aluno.java
@@ -0,0 +1,25 @@
+public class Aluno {
+    String nome;
+    String respostas;
+
+    public Aluno(String nome , String respostas) {
+        this.nome = nome;
+        this.respostas = respostas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRespostas() {
+        return respostas;
+    }
+
+    public void setRespostas(String respostas) {
+        this.respostas = respostas;
+    }
+}

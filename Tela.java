daf58ebc93d7484445ceae9bab0377bdfc5b09f4
renan lima <renanrod2403@gmail.com>
2023-06-10 17:28:16 -0300

    clearConsole() added

--- Tela.java
+++ Tela.java
@@ -46,4 +46,22 @@ public class Tela {
 
         return aluno;
     }
+
+    public void ClearConsole(){
+        try{
+            String operatingSystem = System.getProperty("os.name");
+            if(operatingSystem.contains("Windows")){
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+    
+                startProcess.waitFor();
+            } 
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }

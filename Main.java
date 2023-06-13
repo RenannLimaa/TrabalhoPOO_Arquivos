8f252fa8cfeee64826e7ac882ab9337efc03eb50
renan lima <renanrod2403@gmail.com>
2023-06-12 21:08:21 -0300

    caso mostrarAlunos added

--- Main.java
+++ Main.java
@@ -1,40 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Crud teste = new Crud();
-        Disciplina algebra = new Disciplina("Algebra");
-        /* 
-        Aluno aluno1 = new Aluno("Emanuel", "VVVVVFFFFF");
-        Aluno aluno2 = new Aluno("Gabriel", "FFFFFVVVVV");
-        Aluno aluno3 = new Aluno("Diego", "VVVVVVVFFF");
+        String nomeDisciplina;
+        Disciplina disciplina;
+        Scanner input = new Scanner(System.in);
 
-        algebra.addAluno(aluno1);
-        algebra.addAluno(aluno2);
-        algebra.addAluno(aluno3);
+        System.out.println("Digite o nome da disciplina que deseja acessar");
+        nomeDisciplina = input.nextLine();
+        disciplina = new Disciplina(nomeDisciplina);
 
-        teste.createFile("Algebra");
-        if(teste.createStudent(algebra, aluno1)){
-            System.out.println("aluno criado com sucesso");
 
+        teste.readFile(disciplina);
+        for(Aluno aluno: disciplina.getAlunos()) {
+            System.out.println(aluno.getNome());
         }
-        if(teste.createStudent(algebra, aluno2)){
-            System.out.println("aluno criado com sucesso");
-
-        }
-        if(teste.createStudent(algebra, aluno3)){
-            System.out.println("aluno criado com sucesso");
-
-
-        }   */
         
-          teste.readFile(algebra);
-
-        for (Aluno aluno: algebra.getAlunos()) {
-            System.out.println(aluno.getNome() +" "+aluno.getResposta());
-        } 
-
-        algebra.gerarLista();
-    
+        input.close(); 
+        /* O código não vai ficar assim na main original, mas como fiz sem o código atualizado, precisei fazer assim */
     }
     
 }  

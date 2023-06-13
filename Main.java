a4a48ef7f0cd56e40f658f01b6d6df17a5618c1c
yuriolv <yuri_2505@icloud.com>
2023-06-13 13:39:43 -0300

    alterando layout de tela e corrigindo bugs de cadastrar aluno

--- Main.java
+++ Main.java
@@ -4,46 +4,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Tela tela = new Tela();
-        Crud crud = new Crud();
-
         Disciplina disciplina;
-        Aluno aluno;
-        File file;
-
         Scanner input = new Scanner(System.in);
-        String nomeDisciplina;
-        Integer comando;
+        int comando;
+
+        do {
+
+            tela.ClearConsole();
+            System.out.println("------------------- Bem vindo ao Sistema de Gabarito UECE -------------------\n\n");
+            tela.mostrarTelaInicial();
+    
+            System.out.println("Digite o número respectivo ao que você deseja fazer: ");
+            comando = input.nextInt();
+            clearBuffer(input);
+            tela.ClearConsole();
+    
+    
+            switch(comando) {
+                case 1: 
+                    tela.criarDisciplina();
+                    break;
+    
+                case 2: 
+                    disciplina = tela.selecionarDisciplina();
+                    tela.mostrarMenuDisciplina(disciplina);
+                    break;
+                case 3: 
+                    comando = -1;
+                    break;
+            }
+            
+        } while (comando != -1);
         
-        tela.ClearConsole();
-        System.out.println("Bem vindo ao Sistema de Gabarito UECE");
-        tela.mostrarTelaInicial();
-
-        System.out.println("Digite o número respectivo ao que você deseja fazer");
-        comando = input.nextInt();
-        clearBuffer(input);
-        tela.ClearConsole();
-
-
-        switch(comando) {
-            case 1: 
-                System.out.println("Digite o nome da disciplina que deseja adicionar");
-
-                nomeDisciplina = input.nextLine();
-                disciplina = new Disciplina(nomeDisciplina);
-                crud.createFile(nomeDisciplina);
-                break;
-
-            case 2: 
-                tela.selecionarDisciplina();
-                break;
-            case 3: 
-                System.exit(0);
-        }
-
-
-
-
-
     
     }
     private static void clearBuffer(Scanner scanner) {
--- Tela.java
+++ Tela.java
@@ -1,15 +1,31 @@
 import java.io.File;
+import java.util.Dictionary;
 import java.util.Scanner;
 
 public class Tela {
-    Scanner input = new Scanner(System.in);
-    File file;
-    Crud crud = new Crud();
+    private Scanner input = new Scanner(System.in);
+    private File file;
+    private Crud crud = new Crud();
+    private Disciplina disciplina;
+    private String nomeDisciplina;
+    private Aluno aluno;
 
     public void mostrarTelaInicial() {
         System.out.println("1. Adicionar disciplina");
         System.out.println("2. Consultar disciplinas");
         System.out.println("3. Sair do programa");  // System.exit();
+    }
+
+    public void criarDisciplina(){
+
+        System.out.println("Digite o nome da disciplina que deseja adicionar");
+
+        nomeDisciplina = input.nextLine();
+        disciplina = new Disciplina(nomeDisciplina);
+        crud.createFile(nomeDisciplina);
+
+        System.out.println("Disciplina criada com sucesso.");
+        mostrarTransição(2000);
     }
 
     public void mostrarDisciplinas() {
@@ -20,29 +36,31 @@ public class Tela {
             System.out.println("O diretorio esta vazio");
 
         else {
+            System.out.println("            DISCIPLINAS");
+            System.out.println("------------------------------------");
             for(File arquivo: listaDeArquivos) {
                 System.out.println(arquivo.getName());
+                System.out.println("------------------------------------");
             }
         }
     }
 
-    public void mostrarTelaDisciplina(String nomeDisciplina) {
-        System.out.println("--------------------------"+ nomeDisciplina+ "--------------------------");
+    public void mostrarMenuDisciplina(String nomeDisciplina) {
+        System.out.println("-------------------------- "+ nomeDisciplina+ " --------------------------");
 
-        System.out.println("1. Matricular aluno ");
+        System.out.println("\n\n1. Matricular aluno ");
         System.out.println("2. Mostrar alunos matriculados");
         System.out.println("3. Mostrar resultados da disciplina");
         System.out.println("4. Voltar para tela inicial");
     }
 
-    public void selecionarDisciplina() {
-        String nomeDisciplina;
+    public Disciplina selecionarDisciplina() {
         Scanner input = new Scanner(System.in);
         Disciplina disciplina;
-        Integer comando;
 
+        System.out.println("Disciplinas cadastradas no sistema: \n");
         mostrarDisciplinas();
-        System.out.println("Digite o nome da disciplina que deseja consultar");
+        System.out.println("\nDigite o nome da disciplina que deseja consultar: ");
         nomeDisciplina = input.nextLine();
         ClearConsole();
 
@@ -50,47 +68,84 @@ public class Tela {
         file = new File("Data/" + nomeDisciplina);
 
         if(file.exists() == false) {
-            return;
+            return null;
         }
         
         crud.readFile(disciplina);
-        mostrarTelaDisciplina(nomeDisciplina);
 
-        System.out.println("Digite o numero respectivo ao que você deseja fazer");
-        comando = input.nextInt();
-        ClearConsole();
-
-        switch (comando) {
-            case 1:
-                mostrarTelaAluno();                        
-                
-                break;
-            case 2:
-                for(Aluno newAluno: disciplina.getAlunos()) {
-                    System.out.println(newAluno.getNome());
-                }
-
-                break;
-            
-            case 3: 
-                
-                break;
-        
-            case 4: 
-                
-            default:
-                break;
-            
-        }
+        return disciplina;
 
     }
 
-    public Aluno mostrarTelaAluno() {
+    public void mostrarMenuDisciplina(Disciplina disciplina){
+        int comando = -1;
+
+
+        while(comando != 4){
+            ClearConsole();
+            mostrarMenuDisciplina(disciplina.getNome());
+    
+            System.out.println("Digite o numero respectivo ao que você deseja fazer");
+            comando = input.nextInt();
+            ClearConsole();
+
+            switch (comando) {
+            case 1:
+                aluno = cadastrarAluno();   
+                crud.createStudent(disciplina, aluno);                   
+                
+                break;
+            case 2:
+                crud.readFile(disciplina);
+                mostrarAlunos(disciplina);
+                mostrarTransição(3000);
+                break;
+            
+            case 3: 
+
+                if(disciplina.getAlunos().isEmpty()){
+                    System.out.println( disciplina.getNome() +" não possui alunos matriculados");
+                    break;
+                }
+                crud.readFile(disciplina);
+                disciplina.gerarLista();
+                disciplina.gerarRanking();
+                System.out.println("Arquivos com resultados gerados com sucesso.");
+                mostrarTransição(2000);
+                break;
+        
+            default:
+                
+                break;
+            
+        }
+        }
+        
+    }
+
+    public void mostrarAlunos(Disciplina disciplina){
+        if(disciplina.getAlunos().isEmpty()){
+                    System.out.println( disciplina.getNome() +" não possui alunos matriculados");
+                    return;
+        }
+
+        System.out.println("                    ALUNOS");
+        System.out.println("---------------------------------------------");
+
+        for(Aluno aluno: disciplina.getAlunos()) {
+            System.out.println(aluno.getNome());
+            System.out.println("---------------------------------------------");
+        }
+    }
+
+    public Aluno cadastrarAluno() {
         Aluno aluno;
         String nome, respostas;
-        System.out.println("Digite o nome do aluno");
+        System.out.println("Digite o nome do aluno: ");
        
+        clearBuffer(input);
         nome = input.nextLine();
+        
 
         System.out.println("Digite o cartao resposta do aluno:");
         respostas = input.nextLine();
@@ -116,6 +171,20 @@ public class Tela {
             } 
         }catch(Exception e){
             System.out.println(e);
+        }
+    }
+
+    public void mostrarTransição(int tempo){
+        try {
+            Thread.sleep(tempo);
+        } catch (InterruptedException ex) {
+            // TODO: handle exception
+        }
+    }
+
+    private static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
         }
     }
 }
